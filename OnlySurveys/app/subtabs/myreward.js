ff6226860b8a80966394451967cef152249ad499
Benwong410 <bw410@connect.hku.hk>
2023-11-18 20:48:41 +0800

    fix reward history

--- OnlySurveys/app/subtabs/myreward.js
+++ OnlySurveys/app/subtabs/myreward.js
@@ -8,26 +8,29 @@ import {
   // Button,
   ImageBackground,
   TouchableOpacity,
+  ScrollView
 } from "react-native";
 import { Button, TextInput, List, Card} from "react-native-paper";
 import { MaterialIcons, MaterialCommunityIcons, Ionicons, FontAwesome } from '@expo/vector-icons';
 
 const db = [
     {
+      id: 1,
       name: 'Experiment 1',
       // img: require('../../assets/img/richard.jpg')
       description:  'This is Experiment 1',
       eligibility: '18-25 year olds',
-      reward: '$50 coupon',
+      reward: '$50 HKD',
       unit: 'Department of Psychology',
       hrec: 'EA1000000',
       contact: ['Mr AT', '13728943'],
       link: 'https://forms.office.com/r/0h01kcnfMn?origin=lprLink',
       fav: false,
       chat: true,
-      reward: true
+      is_reward: true
     },
     {
+      id: 2,
       name: 'Experiment 2',
       // img: require('../../assets/img/erlich.jpg')
       description:  'This is Experiment 2',
@@ -39,9 +42,10 @@ const db = [
       link: 'https://forms.office.com/r/0h01kcnfMn?origin=lprLink',
       fav: false,
       chat: true,
-      reward: true
+      is_reward: true
     },
     {
+      id: 3,
       name: 'Experiment 3',
       // img: require('../../assets/img/monica.jpg')
       description:  'This is Experiment 3',
@@ -53,36 +57,8 @@ const db = [
       link: 'https://forms.office.com/r/0h01kcnfMn?origin=lprLink',
       fav: false,
       chat: true,
-      reward: true
+      is_reward: false
     },
-    {
-      name: 'Experiment 4',
-      // img: require('../../assets/img/jared.jpg')
-      description:  'This is Experiment 4',
-      eligibility: 'If you suffer from insomnia for more than 3 months',
-      reward: '$30 HKD',  
-      unit: 'Faculty of Education',
-      hrec: 'EA4000000',
-      contact: ['Ms qwerty', '09876543'],
-      link: 'https://forms.office.com/r/0h01kcnfMn?origin=lprLink',
-      fav: false,
-      chat: true,
-      reward: true
-    },
-    {
-      name: 'Experiment 5',
-      // img: require('../../assets/img/dinesh.jpg')
-      description:  'This is Experiment 5',
-      eligibility: 'Native English speakers',
-      reward: '$50 HKD and a keychain',  
-      unit: 'Department of Linguistics',
-      hrec: 'EA5000000',
-      contact: ['Ms Chi Wah', '12345678'],
-      link: 'https://forms.office.com/r/0h01kcnfMn?origin=lprLink',
-      fav: false,
-      chat: true,
-      reward: true
-    }
   ]
 
 export default function ProfileScreen() {
@@ -95,17 +71,20 @@ export default function ProfileScreen() {
         </View>
         {/* To do: Account History */}
         <Text style = {styles.availableText}>Account History</Text>
-        <View style={styles.cardContainer}>
-        {db.map((data) =>
-          <Card key={data.name} onSwipe={(dir) => {swiped(dir, data.name); onShowSnackBar(); _storeData(data);_removeData(data.name, dir);}} onCardLeftScreen={() => {outOfFrame(data.name); }}>
-            <View style={styles.card}>
-              <Text style={{fontSize: 24}}>{data.name}</Text>
-                <List.Item title="Reward" description={data.reward} left={props => <Ionicons name="md-wallet" size={24} color="#8AC83F" />}/>
-                <List.Item title="Offering Faculty/Department" description={data.unit} left={props => <MaterialCommunityIcons name="offer" size={24} color="#8AC83F" />}/>
-            </View>
-          </Card>
-        )}
-      </View>
+        <ScrollView> 
+          {db.map((data) => ( 
+            <View key={data.id}> 
+              <View style={styles.summary}> 
+                <Text style={styles.summaryText}> 
+                  <List.Item title="Title" description={data.name} left={props => <MaterialIcons name="description" size={24} color="#8AC83F" />}/>
+                  <List.Item title="Reward" description={data.reward} left={props => <Ionicons name="md-wallet" size={24} color="#8AC83F" />}/>
+                  <List.Item title="Offering Faculty/Department" description={data.unit} left={props => <MaterialCommunityIcons name="offer" size={24} color="#8AC83F" />}/>
+                  <Text style={styles.amount}> {data.reward}</Text> 
+                </Text> 
+              </View> 
+            </View> 
+          ))} 
+        </ScrollView> 
     </View> 
   );
 }
@@ -139,39 +118,26 @@ const styles = StyleSheet.create({
         fontSize: 20,
         fontWeight: 'bold',
     },
-    cardContainer: {
-        width: '85%',
-        height: 300,
-      },
-      card: {
-        position: 'absolute',
-        backgroundColor: 'white',
-        // set border color to black
-        borderColor: '#8AC83F',
-        // set border width to 2px
-        borderWidth: 3,
-        width: '100%',
-        height: 200,
-        shadowColor: '#8AC83F',
-        shadowOpacity: 0.2,
-        shadowRadius: 10,
-        borderRadius: 20,
-        resizeMode: 'cover',
-      },
-      cardImage: {
-        width: '100%',
-        height: '100%',
-        overflow: 'hidden',
-        borderRadius: 20,
-        position: 'relative',
-        left: 10,
-        overflow: 'auto',
-      },
-      cardTitle: {
-        position: 'absolute',
-        bottom: 0,
-        margin: 10,
-        color: '#8AC83F',
-      },
+    summaryText: { 
+      fontFamily: "openSansBold", 
+      fontSize: 18, 
+    }, 
+    amount: { 
+      color: "#C2185B", 
+    }, 
+    summary: { 
+      flexDirection: "row", 
+      alignItems: "center", 
+      justifyContent: "space-between", 
+      marginBottom: 20, 
+      padding: 10, 
+      shadowColor: "black", 
+      shadowOpacity: 0.26, 
+      shadowOffset: { width: 0, height: 2 }, 
+      shadowRadius: 8, 
+      elevation: 5, 
+      borderRadius: 10, 
+      backgroundColor: "white", 
+    }, 
   
 });
