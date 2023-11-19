9c8ecdd99037ec1e2cf8050e3a0a6d61982379d9
DethCubeHax <ultimate.nafis.bhadra11@gmail.com>
2023-11-19 20:35:46 +0800

    Improved the chatroom UI

--- OnlySurveys/app/subtabs/chatroom.js
+++ OnlySurveys/app/subtabs/chatroom.js
@@ -4,11 +4,20 @@ import { View, Text, TextInput, Button, StyleSheet, ScrollView } from 'react-nat
 export default function Chatroom({ route, navigation }) {
   const { chatNum } = route.params;
   const [message, setMessage] = useState('');
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState([
+    { text: "Hello, I heard you're conducting a research on mobile applications. I'm interested to participate.", timestamp: '09:00 AM', date: new Date(2023, 10, 18).toLocaleDateString(), sender: 'self' },
+    { text: "Hi there! Yes, we are. Your participation would be greatly appreciated. The survey is about user experiences with mobile applications. Shall we get started?", timestamp: '09:15 AM', date: new Date(2023, 10, 18).toLocaleDateString(), sender: 'other' },
+    { text: "Sounds good. Let's start.", timestamp: '09:30 AM', date: new Date(2023, 10, 18).toLocaleDateString(), sender: 'self' },
+    { text: "Perfect! So, the first question - Do you primarily use Android or iOS?", timestamp: '09:45 AM', date: new Date(2023, 10, 19).toLocaleDateString(), sender: 'other' },
+    { text: "I primarily use Android.", timestamp: '10:00 AM', date: new Date(2023, 10, 19).toLocaleDateString(), sender: 'self' },
+  ]);
 
   const sendMessage = () => {
     if (message.length > 0) {  // Don't send an empty message
-      setMessages(prevMessages => [...prevMessages, message]);
+      const now = new Date();
+      const timestamp = now.toLocaleString([], { hour: '2-digit', minute: '2-digit' });
+      const date = now.toLocaleDateString();
+      setMessages(prevMessages => [...prevMessages, { text: message, timestamp, date, sender: 'self' }]);
       setMessage('');  // Clear the input field
     }
   };
@@ -17,8 +26,16 @@ export default function Chatroom({ route, navigation }) {
     <View style={styles.container}>
       <View style={styles.separator} lightColor="#8AC83F" darkColor="#8AC83F" />
       <ScrollView>
-        {messages.map((msg, index) => (
-          <Text key={index} style={styles.message}>{msg}</Text>
+        {messages.map((msg, index, array) => (
+          <View key={index}>
+            {(index === 0 || msg.date !== array[index - 1].date) && (
+              <Text style={styles.date}>{msg.date}</Text>
+            )}
+            <View style={msg.sender === 'self' ? styles.messageContainer : styles.receivedMessageContainer}>
+              <Text style={styles.message}>{msg.text}</Text>
+              <Text style={styles.timestamp}>{msg.timestamp}</Text>
+            </View>
+          </View>
         ))}
       </ScrollView>
       <View style={styles.inputContainer}>
@@ -49,9 +66,53 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  date: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 20,
+    backgroundColor: '#EDF6C7',
+    width: 'min-content',
+    alignSelf: 'center',
+    padding: '5px',
+    borderRadius: '5px',
+    marginBottom: 10,
+  },
+  messageContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+    backgroundColor: '#8AC83F',
+    padding: 10,
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderRadius: 15,
+    maxWidth: '70%',
+    alignSelf: 'flex-start',
+    marginLeft: 20,
+  },
+  receivedMessageContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+    backgroundColor: '#EDFFD2',
+    padding: 10,
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderRadius: 15,
+    maxWidth: '70%',
+    alignSelf: 'flex-end',
+    marginRight: 20,
+  },
   message: {
     fontSize: 16,
-    marginVertical: 10,
+    marginRight: 10,
+  },
+  timestamp: {
+    fontSize: 12,
+    color: '#666',
+    display: 'flex',
+    flexDirection: 'row',
+    minWidth: 'fit-content',
   },
   inputContainer: {
     flexDirection: 'row',
@@ -60,7 +121,7 @@ const styles = StyleSheet.create({
   input: {
     flex: 1,
     borderWidth: 1,
-    borderColor: '#000',
+    borderColor: '#fff',
     marginRight: 10,
     padding: 10,
     borderRadius: 5,
