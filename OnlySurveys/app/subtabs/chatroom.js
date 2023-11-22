1b89240cc40a87c5894577a42f44a2730a425a5c
DethCubeHax <ultimate.nafis.bhadra11@gmail.com>
2023-11-22 21:29:29 +0800

    Revert "Merge pull request #4 from jamesolano2001/description"
    This reverts commit b143d73aa7a138d6d10da0c01f040d375de75ce2, reversing
    changes made to b0dda7862c2216193e39a81d4dc4288a8f791503.

--- OnlySurveys/app/subtabs/chatroom.js
+++ OnlySurveys/app/subtabs/chatroom.js
@@ -4,20 +4,14 @@ import { View, Text, TextInput, Button, StyleSheet, ScrollView } from 'react-nat
 export default function Chatroom({ route, navigation }) {
   const { chatNum } = route.params;
   const [message, setMessage] = useState('');
-  const [messages, setMessages] = useState([
-    { text: "Hello, I heard you're conducting a research on mobile applications. I'm interested to participate.", timestamp: '09:00 AM', date: new Date(2023, 10, 18).toLocaleDateString(), sender: 'self' },
-    { text: "Hi there! Yes, we are. Your participation would be greatly appreciated. The survey is about user experiences with mobile applications. Shall we get started?", timestamp: '09:15 AM', date: new Date(2023, 10, 18).toLocaleDateString(), sender: 'other' },
-    { text: "Sounds good. Let's start.", timestamp: '09:30 AM', date: new Date(2023, 10, 18).toLocaleDateString(), sender: 'self' },
-    { text: "Perfect! So, the first question - Do you primarily use Android or iOS?", timestamp: '09:45 AM', date: new Date(2023, 10, 19).toLocaleDateString(), sender: 'other' },
-    { text: "I primarily use Android.", timestamp: '10:00 AM', date: new Date(2023, 10, 19).toLocaleDateString(), sender: 'self' },
-  ]);
+  const [messages, setMessages] = useState([]);
 
   const sendMessage = () => {
     if (message.length > 0) {  // Don't send an empty message
-      const now = new Date();
-      const timestamp = now.toLocaleString([], { hour: '2-digit', minute: '2-digit' });
-      const date = now.toLocaleDateString();
-      setMessages(prevMessages => [...prevMessages, { text: message, timestamp, date, sender: 'self' }]);
+      setMessages(prevMessages => [...prevMessages, message]);
+      setTimeout(() => {
+        setMessages(prevMessages => [...prevMessages, "Thank you for contacting us! We will review your message soon!"]);
+      }, 1000);  // 1000 milliseconds = 1 second
       setMessage('');  // Clear the input field
     }
   };
@@ -26,16 +20,8 @@ export default function Chatroom({ route, navigation }) {
     <View style={styles.container}>
       <View style={styles.separator} lightColor="#8AC83F" darkColor="#8AC83F" />
       <ScrollView>
-        {messages.map((msg, index, array) => (
-          <View key={index}>
-            {(index === 0 || msg.date !== array[index - 1].date) && (
-              <Text style={styles.date}>{msg.date}</Text>
-            )}
-            <View style={msg.sender === 'self' ? styles.messageContainer : styles.receivedMessageContainer}>
-              <Text style={styles.message}>{msg.text}</Text>
-              <Text style={styles.timestamp}>{msg.timestamp}</Text>
-            </View>
-          </View>
+        {messages.map((msg, index) => (
+          <Text key={index} style={styles.message}>{msg}</Text>
         ))}
       </ScrollView>
       <View style={styles.inputContainer}>
@@ -66,53 +52,9 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
-  date: {
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginTop: 20,
-    backgroundColor: '#EDF6C7',
-    width: 'min-content',
-    alignSelf: 'center',
-    padding: '5px',
-    borderRadius: '5px',
-    marginBottom: 10,
-  },
-  messageContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 20,
-    backgroundColor: '#8AC83F',
-    padding: 10,
-    paddingLeft: 15,
-    paddingRight: 15,
-    borderRadius: 15,
-    maxWidth: '70%',
-    alignSelf: 'flex-start',
-    marginLeft: 20,
-  },
-  receivedMessageContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 20,
-    backgroundColor: '#EDFFD2',
-    padding: 10,
-    paddingLeft: 15,
-    paddingRight: 15,
-    borderRadius: 15,
-    maxWidth: '70%',
-    alignSelf: 'flex-end',
-    marginRight: 20,
-  },
   message: {
     fontSize: 16,
-    marginRight: 10,
-  },
-  timestamp: {
-    fontSize: 12,
-    color: '#666',
-    display: 'flex',
-    flexDirection: 'row',
-    minWidth: 'fit-content',
+    marginVertical: 10,
   },
   inputContainer: {
     flexDirection: 'row',
@@ -121,7 +63,7 @@ const styles = StyleSheet.create({
   input: {
     flex: 1,
     borderWidth: 1,
-    borderColor: '#fff',
+    borderColor: '#000',
     marginRight: 10,
     padding: 10,
     borderRadius: 5,
